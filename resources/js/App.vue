5c8a4d8b74f79b8258d3b6203895c300d3b37202
wendia <wendia1234qwer@mail.ru>
2024-03-15 23:43:56 +0300

    init

---
+++ resources/js/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1 class="text-center">Laravel sanctum</h1>
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+    .container {
+        background: #ffffff;
+        border-radius: 10px;
+        padding-top: 20px;
+        padding-bottom: 700px;
+        margin-top: 30px;
+        margin-bottom: 30px;
+    }
+</style>
